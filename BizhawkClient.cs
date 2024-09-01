397d6a7f84293b0905041a1d033b49c952bb6fec
Thepharaoh49 <Thepharaoh49@hotmail.co.uk>
2024-09-01 11:56:15 +0100

    Initial Commit

---
+++ BizhawkClient.cs
@@ -0,0 +1,31 @@
+﻿using Archipelago.Core;
+using Archipelago.Core.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archipelago.Bizhawk
+{
+    public class BizhawkClient : IGameClient
+    {
+        public bool IsConnected { get; set; }
+        public int ProcId { get; set; } = Memory.BIZHAWK_PROCESSID;
+        public BizhawkClient()
+        {
+
+        }
+        public bool Connect()
+        {
+            Console.WriteLine("Connecting to Bizhawk");
+            var pid = Memory.BIZHAWK_PROCESSID;
+            if (pid == 0)
+            {
+                Console.WriteLine("Bizhawk not found.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
